f3444f8c37d6cd221bec6aad6c156375f052ece5
Dren2112 <98460754+Dren2112@users.noreply.github.com>
2022-05-22 19:17:20 +0100

    Button implementation

--- scripts/TokenRollPrivacy.js
+++ scripts/TokenRollPrivacy.js
@@ -1,6 +1,7 @@
 class TokenRollPrivacy extends FormApplication{
 
     static _initButton(app, html, data) {
+        console.log("Token Roll Privacy Init")
         let diaBtn = $(`<a class="open-dia" title="Roll Privacy" ></a>`)
         diaBtn.onclick = console.log("Button Clicked Woo!")
 
