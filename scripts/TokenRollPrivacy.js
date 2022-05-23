15a005c4b76f5da3914fa52242127d55430a88e7
Dren2112 <98460754+Dren2112@users.noreply.github.com>
2022-05-23 15:03:43 +0100

    Dialog prompt testing

--- scripts/TokenRollPrivacy.js
+++ scripts/TokenRollPrivacy.js
@@ -1,45 +1,29 @@
 console.log("Token Roll Privacy Init")
 
 
-const dia = new Dialog({
-    title: "Roll Privacy",
-    content: 'Select the default roll privacy for this actor',
-    buttons: {
-        none: {
-            label: "Default",
-            callback: () => {
-                console.log("default selected")
-            }
-        },
-        Private_GM: {
-            label: "Private GM",
-            callback: () => {
-                console.log("Private GM selected")
-            }
-        },
-        Blind: {
-            label: "Blind GM",
-            callback: () => {
-                console.log("Blind GM selected")
-            }
-        },
-        self: {
-            label: "Self",
-            callback: () => {
-                console.log("Self selected")
-            }
-        }
-    },
-        default: "none",
 
-});
 
 
 
 class TokenRollPrivacy extends FormApplication {
 
-    static _initButton(app, html, data) {
+    static remove(ID, Array){
+        let Index = Array.indexOf(ID)
 
+
+        if(Index > -1){
+            Array.splice(pIndex, 1)
+        }
+
+        return Array;
+    }
+
+    static _initButton(app, html, data) {
+        let Private = [];
+        let Blind = [];
+        let Self = [];
+
+        let ID = app.appId;
         let diaBtn = $(`<a class="open-dia" title="Roll Privacy" ><i class ="fas fa-dice-d20"></i> Roll Privacy </a>`)
         diaBtn.click(ev => {
             dia.render(true);
@@ -47,7 +31,52 @@ class TokenRollPrivacy extends FormApplication {
 
         let titleElement = html.closest('.app').find('.window-title');
         diaBtn.insertAfter(titleElement);
+
+        const dia = new Dialog({
+            title: "Roll Privacy",
+            content: 'Select the default roll privacy for this actor',
+            buttons: {
+                none: {
+                    label: "Default",
+                    callback: () => {
+                        Private =this.remove(ID, Private)
+                        Blind =this.remove(ID, Blind)
+                        Self =this.remove(ID, Self)
+                    }
+                },
+                Private_GM: {
+                    label: "Private GM",
+                    callback: () => {
+                        Private =this.remove(ID, Private)
+                        Blind =this.remove(ID, Blind)
+                        Self =this.remove(ID, Self)
+
+                        Private.push(ID);
+                        console.log(Private[0])
+                    }
+                },
+                Blind: {
+                    label: "Blind GM",
+                    callback: () => {
+                        Private =this.remove(ID, Private)
+                        Blind =this.remove(ID, Blind)
+                        Self =this.remove(ID, Self)
+
+                        Blind.push(ID);
+                    }
+                },
+                self: {
+                    label: "Self",
+                    callback: () => {
+                        Self.push(ID);
+                    }
+                }
+            },
+            default: "none",
+
+        });
     }
+
 }
 
     Hooks.on('renderActorSheet', (app, html, data) => {
